6f96d0022fa0238bbf52e032fcf545c77955ff6b
natthaphong <s6704062611336@email.kmutnb.ac.th>
2025-12-23 20:32:36 +0700

    Add files via upload

---
+++ lab2expr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void){
+    float a,b,c,d;
+    printf("a=1, b=1, c=5, d=1;");
+    a=1, b=1, c=5, d=1;
+    printf("%.1f\n",  (3*(a/5)) + 1/b);
+    printf("%.1f\n", ((3*a) + (5*b)) / (2+c));
+    printf("%.1f \n",    (2.0 / 7.0) * ((pow(4, 3+ c)) - 5 * d));
+    printf("%.1f \n" , sqrt( (2+8*b) /a ) );
+    printf("%.1f" , cbrt(pow(b,2) - 4*d) );
+    printf("\n");
+    printf("a=9, b=2, c=0, d=1;");
+    a=9, b=2, c=0, d=1;
+    printf("%.1f\n",  (3*(a/5)) + 1/b);
+    printf("%.1f\n", ((3*a) + (5*b)) / (2+c));
+    printf("%.1f \n",    (2.0 / 7.0) * ((pow(4, 3+ c)) - 5 * d));
+    printf("%.1f \n" , sqrt( (2+8*b) /a ) );
+    printf("%.1f" , cbrt(pow(b,2) - 4*d) );
+    printf("\n");
+    printf("a=0, b=3, c=3, d=3;");
+    a=0, b=3, c=3, d=3;
+    printf("%.1f\n",  (3*(a/5)) + 1/b);
+    printf("%.1f\n", ((3*a) + (5*b)) / (2+c));
+    printf("%.1f \n",    (2.0 / 7.0) * ((pow(4, 3+ c)) - 5 * d));
+    printf("%.1f \n" , sqrt( (2+8*b) /a ) );
+    printf("%.1f" , cbrt(pow(b,2) - 4*d) );
+    
+}
